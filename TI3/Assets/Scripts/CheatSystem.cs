23d5c92a1a46ba5fde722f5a1251892f6dedf6d4
AndreGabrielCastro <109389758+AndreGabrielCastro@users.noreply.github.com>
2023-06-25 08:52:02 -0300

    2023.06.25 added some cheats

---
+++ TI3/Assets/Scripts/CheatSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatSystem : MonoBehaviour
+{
+    public void CheatAddGearcoin(int value)
+    {
+        PlayerSystem.Instance.AlterateGearcoins(value);
+    }
+    public void CheatAddInformation(int value)
+    {
+        PlayerSystem.Instance.AlterateInformation(value);
+    }
+}
